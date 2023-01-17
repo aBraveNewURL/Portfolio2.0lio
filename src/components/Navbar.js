4c658369f2ba1f1676a8ada7c0bfe48d47a24c55
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-16 20:24:29 -0700

    fixed navbar overlap, styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,16 @@ const Navbar = () => {
     const handleClick = () => setClick(!click);
     const [color, setColor] = useState(false);
     const changeColor = () => {
-        if (window.scrollY >=100){
+        if (window.scrollY >= 100) {
             setColor(true);
         } else {
             setColor(false);
         }
     };
 
-    window.addEventListener()
+    window.addEventListener("scroll", changeColor);
     return (
-        <div className="header">
+        <div className={color ? "header header-bg" : "header"}>
             <Link to="/">
                 <h1>Portfolio2.0</h1>
             </Link>
