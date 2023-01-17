753ad02f583b475f0ab6c3bbf91bc500f2f2b67a
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-17 16:43:56 -0700

    added project data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -4,6 +4,24 @@ import weather from "../assets/weather"
 
 const ProjectCardData = [
     {
-        imgsrc: { fuelfiend }
+        imgsrc: { fuelfiend },
+        title: "Fuel Fiend",
+        text: "Fuel Fiend calculates your fuel costs based on vehicle details and location data!",
+        view: "https://github.com/Team2Baybee/Fuel-Fiend",
+        source: "https://team2baybee.github.io/Fuel-Fiend/"
+    },
+    {
+        imgsrc: { bingo },
+        title: "Build-a-Bingo",
+        text: "Build and generate your own custom Bingo cards.",
+        view: "https://build-a-bingo.herokuapp.com/",
+        source: "https://github.com/aBraveNewURL/Build_A_Bingo"
+    },
+    {
+        imgsrc: { weather },
+        title: "Forecast Grabber",
+        text: "Display the current weather and 5 day forecast for any city.",
+        view: "https://abravenewurl.github.io/Forecast6rabber/",
+        source: "https://github.com/aBraveNewURL/Forecast6rabber"
     }
 ]
