6c0334b8f9d81ec1324b36e2d88b239cdcea5fe3
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-17 20:39:22 -0700

    added blank contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,21 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className='form'>
+        <form>
+            <label>Your Name</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="email"></input>
+            <label>Subject</label>
+            <input type="text"></input>
+            <label>Message</label>
+            <input type="text"></input>
+            <textarea rows="6" placeholder='Type Your Message Here'></textarea>
+            <button className='btn'>Submit</button>
+            
+        </form>
+    </div>
   )
 }
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import HeroImg2 from '../components/HeroImg2'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="CONTACT" text="How to reach me"/>
+      <Form />
       <Footer />
     </div>
   )
-}
+};
 
-export default Contact
+export default Contact;
