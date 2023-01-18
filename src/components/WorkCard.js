19d99661f013ae2bd58c1c95ae8ffe15ee21f86f
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-17 19:06:04 -0700

    fixed routing and syntax issues

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,17 +1,17 @@
 import "../components/WorkCardStyles.css";
 import React from 'react'
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 const WorkCard = (props) => {
     return (
         <div className="project-card">
-            <img src={props.imgsrc} alt="image" />
+            <img src={props.imgsrc} alt="projectPreview" />
             <h2 className="project-title">{props.title}</h2>
             <div className="pro-details">
                 <p>{props.text}</p>
                 <div className="pro-btns"></div>
-                <NavLink to={props.view} className="btn">View</NavLink>
-                <NavLink to={props.source} className="btn">Source</NavLink>
+                <a href={props.view} className="btn" target="_blank" rel="noreferrer">View</a>
+                <a href={props.source} className="btn" target="_blank" rel="noreferrer">Source</a>
             </div>
         </div>
     )
