eed7c2835eca0a83d1dff9ec6aa8a0701c196e19
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-01-17 23:08:02 -0700

    added preview, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Portfolio2.0lio
-Challenge 20- An updated and refactored personal portfolio leveraging React.js
+Challenge 20 - An updated and refactored personal portfolio leveraging React.js
+
+## Description
+Nearing the end of our course, our cohort was tasked with revamping one of our earliest projects: our own project portfolio. The objective was to implement a top-to-bottom reimagination of our intial project page- this time by employing JavaScript and React.js to create a single-page portfolio that is responsive, data-driven, and polished.
+![Here is a small preview](../preview.jpg)
+
+## Deployed Page
+The page to which the portfolio has been deployed can be reached [here](). 
+Thanks for looking!
--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,8 +1,9 @@
 import '../components/AboutContentStyles.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import top1 from '../assets/top1.jpg';
-import bottom1 from '../assets/bottom1.jpg';
+// import top1 from '../assets/top1.jpg';
+// import bottom1 from '../assets/bottom1.jpg';
+// import mug from '../assets/mug.jpg';
 
 
 
@@ -11,13 +12,14 @@ const AboutContent = () => {
         <div className='about'>
             <div className='left'>
                 <h1>Who Am I?</h1>
-                <p>I am an aspiring front-end developer. I love to create fun and interesting content.</p>
+                <p>I am an aspiring front-end developer who loves to create fun and interesting content.</p>
+                <p>In my spare time I enjoy travelling, trying new foods, and learning about new and exciting technologies.</p>
                 <Link to="/contact">
                     <button className='btn'>Contact</button>
                 </Link>
             </div>
 
-            <div className='right'>
+            {/* <div className='right'>
                 <div className='img-container'>
                     <div className='img-stack top'>
                         <img src={top1} className='img' alt="topRight"></img>
@@ -25,8 +27,9 @@ const AboutContent = () => {
                     <div className='img-stack bottom'>
                         <img src={bottom1} className='img' alt="bottomRight"></img>
                     </div>
+                    
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
