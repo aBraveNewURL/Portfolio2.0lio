76f0aec91fb24050afaefe2be9870281f58628f2
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-03-22 20:15:49 -0600

    reworking form

---
+++ src/components/FormRevamp.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Benjamin Sokolik',
+      subject: subject,
+      message_html: message,
+    };
+
+    emailjs.send(
+      'YOUR_SERVICE_ID',
+      'YOUR_TEMPLATE_ID',
+      templateParams,
+      'YOUR_USER_ID'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Your Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        E-mail:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Subject:
+        <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+      </label>
+      <label>
+        Message:
+        <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default ContactForm;
